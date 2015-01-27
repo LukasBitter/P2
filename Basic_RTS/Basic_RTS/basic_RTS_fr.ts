47d4c92c804c586514f4c38b0d0b8474c059d82d
LukasBitter <lukas.bitter@he-arc.ch>
2015-01-27 10:53:26 +0100

    Traduction fr

--- Basic_RTS/Basic_RTS/basic_RTS_fr.ts
+++ Basic_RTS/Basic_RTS/basic_RTS_fr.ts
@@ -6,18 +6,18 @@
     <message>
         <location filename="GameComponent/EditorInterface/editview.cpp" line="165"/>
         <source>Sauver la map</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauver la map</translation>
     </message>
     <message>
         <location filename="GameComponent/EditorInterface/editview.cpp" line="166"/>
         <location filename="GameComponent/EditorInterface/editview.cpp" line="191"/>
         <source>Nom :</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom :</translation>
     </message>
     <message>
         <location filename="GameComponent/EditorInterface/editview.cpp" line="190"/>
         <source>Ouvrir la map</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvrir la map</translation>
     </message>
 </context>
 <context>
@@ -25,159 +25,159 @@
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="59"/>
         <source>No slot</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de slot</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="61"/>
         <source>Not ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas prêt</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="62"/>
         <source>Ready !</source>
-        <translation type="unfinished"></translation>
+        <translation>Prêt !</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="75"/>
         <source>Your user name: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Votre pseudo: %1</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="107"/>
         <source>ERROR</source>
-        <translation type="unfinished"></translation>
+        <translation>ERREUR</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="117"/>
         <source>Players have the same colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Les joueurs ont la même couleur</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="120"/>
         <source>Players have same spawn</source>
-        <translation type="unfinished"></translation>
+        <translation>Les joueurs ont le même spawn</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="123"/>
         <source>Game is already running</source>
-        <translation type="unfinished"></translation>
+        <translation>Le jeu est déjà lancé</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="126"/>
         <source>A player hasn&apos;t selected any spawn</source>
-        <translation type="unfinished"></translation>
+        <translation>Un joueur n&apos;a pas sélectionné de spawn</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="129"/>
         <source>A player hasn&apos;t selected any colour</source>
-        <translation type="unfinished"></translation>
+        <translation>Un joueurn&apos;a pas sélectionné de couleur</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="132"/>
         <source>Invalid map</source>
-        <translation type="unfinished"></translation>
+        <translation>Map non valide</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="135"/>
         <source>Map is too small</source>
-        <translation type="unfinished"></translation>
+        <translation>La map est trop petite</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="138"/>
         <source>A player is not ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Un joueur n&apos;est pas prêt</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="141"/>
         <source>Lobby is full!</source>
-        <translation type="unfinished"></translation>
+        <translation>Le salon est plein!</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="194"/>
         <source>Enter your user name</source>
-        <translation type="unfinished"></translation>
+        <translation>Saisissez votre pseudo</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="195"/>
         <source>User name :</source>
-        <translation type="unfinished"></translation>
+        <translation>Pseudo:</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="205"/>
         <source>Contains invalid characters</source>
-        <translation type="unfinished"></translation>
+        <translation>Contient des caractères invaides</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="238"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Connecté</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="288"/>
         <source>Launch game</source>
-        <translation type="unfinished"></translation>
+        <translation>Lancer le jeu</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="289"/>
         <source>Connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Connexion</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="290"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Retour</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="291"/>
         <source>Change user name</source>
-        <translation type="unfinished"></translation>
+        <translation>Changer le pseudo</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="292"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Prêt</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="297"/>
         <location filename="GameMenu/lobbymenu.cpp" line="389"/>
         <source>Enter host IP address</source>
-        <translation type="unfinished"></translation>
+        <translation>Saisissez l&apos;adresse IP de l&apos;hôte</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="298"/>
         <location filename="GameMenu/lobbymenu.cpp" line="390"/>
         <source>Your user name : </source>
-        <translation type="unfinished"></translation>
+        <translation>Voter pseudo :</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="299"/>
         <source>Not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Non connecté</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="302"/>
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>Envoyer</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="371"/>
         <source>Red</source>
-        <translation type="unfinished"></translation>
+        <translation>Rouge</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="372"/>
         <source>Green</source>
-        <translation type="unfinished"></translation>
+        <translation>Vert</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="373"/>
         <source>Orange</source>
-        <translation type="unfinished"></translation>
+        <translation>Orange</translation>
     </message>
     <message>
         <location filename="GameMenu/lobbymenu.cpp" line="374"/>
         <source>Yellow</source>
-        <translation type="unfinished"></translation>
+        <translation>Jaune</translation>
     </message>
 </context>
 <context>
@@ -185,22 +185,22 @@
     <message>
         <location filename="GameMenu/welcomemenu.cpp" line="46"/>
         <source>Host</source>
-        <translation type="unfinished"></translation>
+        <translation>Héberger une partie</translation>
     </message>
     <message>
         <location filename="GameMenu/welcomemenu.cpp" line="47"/>
         <source>Join</source>
-        <translation type="unfinished"></translation>
+        <translation>Rejoindre une partie</translation>
     </message>
     <message>
         <location filename="GameMenu/welcomemenu.cpp" line="48"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitter</translation>
     </message>
     <message>
         <location filename="GameMenu/welcomemenu.cpp" line="49"/>
         <source>Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>Editeur de map</translation>
     </message>
 </context>
 </TS>
